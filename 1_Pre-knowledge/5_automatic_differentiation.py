665f997702c006b9a7b1d7bdc74e7960c20db21d
z1178902213 <1178902213@qq.com>
2021-10-08 22:30:53 +0800

    20211008

--- 1_Pre-knowledge/5_automatic_differentiation.py
+++ 1_Pre-knowledge/5_automatic_differentiation.py
@@ -8,7 +8,7 @@ x = torch.arange(4.0)
 print(f'x的值为：{x}')
 # 计算梯度前，需要一个地方存储梯度
 x.requires_grad_(True) # 等价于 `x = torch.arange(4.0, requires_grad=True)`
-print(x.grad) # 不懂这个x.grad是啥，默认值是None
+print(x.grad) # 目前x没有梯度
 # 计算y
 y = 2 * torch.dot(x, x) #计算向量内积
 # 自动计算y关于每个x分量的梯度
@@ -22,4 +22,21 @@ y.backward()
 print(x.grad)
 
 
-# 2、非标量变量的反向传播
+# 2、非标量变量的反向传播(不就是函数求导吗？这有啥问题？)
+# 对非标量调用`backward`需要传入一个`gradient`参数，该参数指定微分函数关于`self`的梯度。在我们的例子中，我们只想求偏导数的和，所以传递一个1的梯度是合适的
+x.grad.zero_()
+y = x * x
+# 等价于y.backward(torch.ones(len(x)))
+y.sum().backward()
+print(x.grad == 2 * x)
+
+
+# 3、分离计算
+# 若y是x的函数，z是x,y的函数，若要计算z的梯度时视y为常数不做梯度计算，可以使用分离计算的技巧
+x.grad.zero_()
+y = x * x
+u = y.detach() # 相当于让y独立了
+z = u * x
+z.sum().backward()
+print(x.grad == u)
+
