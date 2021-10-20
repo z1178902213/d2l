dacc9cc326c6f23463847467f8a5eefaa3e1e878
z1178902213 <1178902213@qq.com>
2021-10-20 08:54:55 +0800

    20211020

--- 5_Deep_learning_computing/5_1.py
+++ 5_Deep_learning_computing/5_1.py
@@ -7,3 +7,18 @@ net = nn.Sequential(nn.Linear(20, 256), nn.ReLU(), nn.Linear(256, 10))
 X = torch.rand(2, 20)
 print(X)
 print(net(X))
+
+
+class MLP(nn.Module):
+    # 用模型参数声明层。这里，我们声明两个全连接的层
+    def __init__(self):
+        # 调用`MLP`的父类`Block`的构造函数来执行必要的初始化。
+        # 这样，在类实例化时也可以指定其他函数参数，例如模型参数`params`（稍后将介绍）
+        super().__init__()
+        self.hidden = nn.Linear(20, 256)  # 隐藏层
+        self.out = nn.Linear(256, 10)  # 输出层
+
+    # 定义模型的正向传播，即如何根据输入`X`返回所需的模型输出
+    def forward(self, X):
+        # 注意，这里我们使用ReLU的函数版本，其在nn.functional模块中定义。
+        return self.out(F.relu(self.hidden(X)))
